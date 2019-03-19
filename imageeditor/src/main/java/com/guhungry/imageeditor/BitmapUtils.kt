bdc9b14d2c43d7e7d608b5a19513236e799e328c
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-03-19 13:07:59 +0700

    [Bitmap Utils] Change: Add crop()

--- imageeditor/src/main/java/com/guhungry/imageeditor/BitmapUtils.kt
+++ imageeditor/src/main/java/com/guhungry/imageeditor/BitmapUtils.kt
@@ -1,5 +1,11 @@
 package com.guhungry.imageeditor
 
+import android.graphics.BitmapRegionDecoder
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.Rect
+import java.io.InputStream
+
 object BitmapUtils {
     /**
      * When scaling down the bitmap, decode only every n-th pixel in each dimension.
@@ -18,5 +24,22 @@ object BitmapUtils {
             }
         }
         return sampleSize
+    }
+
+    /**
+     * Reads and crops the bitmap.
+     * @param outOptions Bitmap options, useful to determine `outMimeType`.
+     */
+    private fun crop(input: InputStream, region: Rect, outOptions: BitmapFactory.Options): Bitmap {
+        input.use {
+            // Efficiently crops image without loading full resolution into memory
+            // https://developer.android.com/reference/android/graphics/BitmapRegionDecoder.html
+            val decoder = BitmapRegionDecoder.newInstance(input, false)
+            try {
+                return decoder.decodeRegion(region, outOptions)
+            } finally {
+                decoder.recycle()
+            }
+        }
     }
 }
