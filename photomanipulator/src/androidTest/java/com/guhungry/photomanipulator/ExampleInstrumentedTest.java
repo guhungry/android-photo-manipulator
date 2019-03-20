08763b3543a21069aab9a9834627abe5b329499d
Woraphot Chokratanasombat <guhungry@gmail.com>
2019-03-21 01:49:27 +0700

    [Android] Fix: Config JUnit 5 properly

--- photomanipulator/src/androidTest/java/com/guhungry/photomanipulator/ExampleInstrumentedTest.java
+++ photomanipulator/src/androidTest/java/com/guhungry/photomanipulator/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.guhungry.imageeditor.test", appContext.getPackageName());
+        assertEquals("com.guhungry.photomanipulator.test", appContext.getPackageName());
     }
 }
