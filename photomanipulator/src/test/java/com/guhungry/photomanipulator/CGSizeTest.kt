247bb8a9ac6f85fd584316ebd79cb6189599d0a7
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-04-30 18:57:42 +0700

    [CGSize] Change: Add test for all method

---
+++ photomanipulator/src/test/java/com/guhungry/photomanipulator/CGSizeTest.kt
@@ -0,0 +1,30 @@
+package com.guhungry.photomanipulator
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+
+internal class CGSizeTest {
+    var sut: CGSize? = null
+
+    @AfterEach
+    fun tearDown() {
+        sut = null
+    }
+
+    @Test
+    fun ratio() {
+        sut = CGSize(800, 600)
+
+        assertThat(sut!!.ratio(), equalTo((4 / 3.0).toFloat()))
+    }
+
+    @Test
+    fun getValues() {
+        sut = CGSize(959, 600)
+
+        assertThat(sut!!.width, equalTo(959))
+        assertThat(sut!!.height, equalTo(600))
+    }
+}
