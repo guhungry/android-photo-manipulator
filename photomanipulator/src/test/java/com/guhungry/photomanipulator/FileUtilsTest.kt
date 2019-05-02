126e00f1a1fa1f20bbc9d312c14773e58e8089ad
Woraphot Chokratanasombat <bee.w@excelbangkok.com>
2019-05-02 11:10:11 +0700

    [FileUtilsTest] Change: Add test for FileUtils.cleanDirectory()

--- photomanipulator/src/test/java/com/guhungry/photomanipulator/FileUtilsTest.kt
+++ photomanipulator/src/test/java/com/guhungry/photomanipulator/FileUtilsTest.kt
@@ -7,10 +7,9 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-
-import org.mockito.Mockito
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import java.io.File
+import java.io.FilenameFilter
 import java.io.IOException
 
 internal class FileUtilsTest {
@@ -27,7 +26,7 @@ internal class FileUtilsTest {
 
     @Test
     fun `cachePath should throw error when all cache dir is null`() {
-        context = Mockito.mock(Context::class.java)
+        context = mock(Context::class.java)
 
         val exception = assertThrows<IOException> { FileUtils.cachePath(context!!) }
 
@@ -37,8 +36,8 @@ internal class FileUtilsTest {
 
     @Test
     fun `cachePath should externalCacheDir when internalCacheDir is null`() {
-        val dir = Mockito.mock(File::class.java)
-        context = Mockito.mock(Context::class.java)
+        val dir = mock(File::class.java)
+        context = mock(Context::class.java)
         `when`(context!!.externalCacheDir).thenReturn(dir)
 
         assertThat(FileUtils.cachePath(context!!)!!, sameInstance(dir))
@@ -46,8 +45,8 @@ internal class FileUtilsTest {
 
     @Test
     fun `cachePath should internalCacheDir when externalCacheDir is null`() {
-        val dir = Mockito.mock(File::class.java)
-        context = Mockito.mock(Context::class.java)
+        val dir = mock(File::class.java)
+        context = mock(Context::class.java)
         `when`(context!!.cacheDir).thenReturn(dir)
 
         assertThat(FileUtils.cachePath(context!!)!!, sameInstance(dir))
@@ -55,12 +54,12 @@ internal class FileUtilsTest {
 
     @Test
     fun `cachePath should externalCacheDir when external has more free space`() {
-        val external = Mockito.mock(File::class.java)
+        val external = mock(File::class.java)
         `when`(external.freeSpace).thenReturn(55555)
-        val internal = Mockito.mock(File::class.java)
+        val internal = mock(File::class.java)
         `when`(internal.freeSpace).thenReturn(30000)
 
-        context = Mockito.mock(Context::class.java)
+        context = mock(Context::class.java)
         `when`(context!!.cacheDir).thenReturn(internal)
         `when`(context!!.externalCacheDir).thenReturn(external)
 
@@ -69,15 +68,26 @@ internal class FileUtilsTest {
 
     @Test
     fun `cachePath should internalCacheDir when external has less free space`() {
-        val external = Mockito.mock(File::class.java)
+        val external = mock(File::class.java)
         `when`(external.freeSpace).thenReturn(1111)
-        val internal = Mockito.mock(File::class.java)
+        val internal = mock(File::class.java)
         `when`(internal.freeSpace).thenReturn(30000)
 
-        context = Mockito.mock(Context::class.java)
+        context = mock(Context::class.java)
         `when`(context!!.cacheDir).thenReturn(internal)
         `when`(context!!.externalCacheDir).thenReturn(external)
 
         assertThat(FileUtils.cachePath(context!!)!!, sameInstance(internal))
+    }
+
+    @Test
+    fun `cleanDirectory should delete file with starts with prefix`() {
+        val directory = mock(File::class.java)
+        val files = arrayOf(mock(File::class.java))
+        `when`(directory.listFiles(any<FilenameFilter>())).thenReturn(files)
+
+        FileUtils.cleanDirectory(directory, "DELETE ME")
+
+        verify(files[0], times(1)).delete()
     }
 }
