e286ebf8ddc5d69abd8081d755c78fbe294dcac6
Woraphot Chokratanasombat <guhungry@gmail.com>
2024-03-23 23:22:29 +0700

    [Example] Change: Update android app template

---
+++ app/src/main/java/com/guhungry/photomanipulator/demo/ImageUtils.kt
@@ -0,0 +1,100 @@
+package com.guhungry.photomanipulator.demo
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import com.guhungry.photomanipulator.BitmapUtils
+import com.guhungry.photomanipulator.CGRect
+import com.guhungry.photomanipulator.CGSize
+import com.guhungry.photomanipulator.FileUtils
+import java.io.InputStream
+import java.util.Locale
+
+object ImageUtils {
+    /**
+     * Get Bitmap from Image Uri
+     *
+     * @param context
+     * @param uri Uri of Image file
+     */
+    @JvmOverloads
+    @JvmStatic fun bitmapFromUri(context: Context, uri: String, options: BitmapFactory.Options? = null): Bitmap {
+        openBitmapInputStream(context, uri).use { image ->
+            val matrix = openBitmapInputStream(context, uri).use {
+                BitmapUtils.getCorrectOrientationMatrix(it)
+            }
+
+            val bitmap: Bitmap = BitmapFactory.decodeStream(image, null, options)!!
+
+            return if (matrix != null) Bitmap.createBitmap(bitmap, 0, 0, bitmap.width,
+                bitmap.height, matrix, true).also { bitmap.recycle()  } else bitmap
+        }
+    }
+
+    @JvmStatic fun mutableOptions() = BitmapFactory.Options().apply { inMutable = true }
+
+    private fun openBitmapInputStream(context: Context, uri: String): InputStream = FileUtils.openBitmapInputStream(context, computeUri(context, uri))
+
+    private fun computeUri(context: Context, uri: String): String {
+        return if (Uri.parse(uri).scheme != null) uri else computeDrawableResourceUri(context, uri)
+    }
+    private fun computeDrawableResourceUri(context: Context, name: String) = "android.resource://${context.packageName}/${computeDrawableResourceId(context, name)}"
+    private fun computeDrawableResourceId(context: Context, name: String): Int {
+        if (name.isEmpty()) return 0
+
+        // name could be a resource id.
+        try {
+            return name.toInt()
+        } catch (e: NumberFormatException) {
+            // Do nothing.
+        }
+
+        val resource = name.lowercase(Locale.getDefault()).replace("-", "_")
+        return context.resources.getIdentifier(resource, "drawable", context.packageName)
+    }
+
+    /**
+     * Get Crop and resize Bitmap from Image Uri
+     *
+     * @param context
+     * @param uri Uri of Image file
+     */
+    @JvmStatic fun cropBitmapFromUri(context: Context, uri: String, cropRegion: CGRect, targetSize: CGSize?): Bitmap {
+        openBitmapInputStream(context, uri).use { image ->
+            val output = if (targetSize == null) BitmapUtils.crop(image, cropRegion, mutableOptions())
+            else {
+                val matrix = openBitmapInputStream(context, uri).use {
+                    BitmapUtils.getCorrectOrientationMatrix(it)
+                }
+
+                BitmapUtils.cropAndResize(image, cropRegion, targetSize, mutableOptions(), matrix)
+            }
+
+            return makeMutable(output)
+        }
+    }
+
+    private fun makeMutable(output: Bitmap): Bitmap {
+        return when {
+            output.isMutable -> output
+            else -> try {
+                output.copy(output.config, true)
+            } finally {
+                output.recycle()
+            }
+        }
+    }
+
+    /**
+     * Save image to temp file
+     *
+     * @param context
+     */
+    @JvmStatic fun saveTempFile(context: Context, image: Bitmap, mimeType: String, prefix: String, quality: Int): String {
+        val file = FileUtils.createTempFile(context, prefix, mimeType)
+        FileUtils.saveImageFile(image, mimeType, quality, file)
+
+        return Uri.fromFile(file).toString()
+    }
+}
--- app/src/main/java/com/guhungry/photomanipulator/demo/MainActivity.kt
+++ app/src/main/java/com/guhungry/photomanipulator/demo/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.guhungry.photomanipulator.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
+import android.os.StrictMode
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.guhungry.photomanipulator.BitmapUtils
+import com.guhungry.photomanipulator.demo.ui.theme.MyApplicationTheme
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+        println("BEE")
+        var bee = ImageUtils.bitmapFromUri(
+            this.applicationContext, "https://github.com/guhungry/react-native-photo-manipulator/blob/34c982bbba3837b32de230d2aad30aae18cf65fd/docs/demo-background.jpg?raw=true", ImageUtils.mutableOptions()
+        )
+        BitmapUtils.printText(bee, "BEE\nhaha", PointF(50f, 50f), Color.BLACK, 40f, rotation = -45f)
+        println(bee)
+        var len = bee.height
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
     }
 }
