523c65872fd8487b8a127c80f821f41d879018a4
Woraphot Chokratanasombat <guhungry@gmail.com>
2024-06-28 22:19:21 +0700

    [Refactor] Change: Explicit declare return type, use MockAndroidFactory

--- photomanipulator/src/androidTest/java/com/guhungry/photomanipulator/TestHelper.kt
+++ photomanipulator/src/androidTest/java/com/guhungry/photomanipulator/TestHelper.kt
@@ -1,13 +1,17 @@
 package com.guhungry.photomanipulator
+
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.annotation.DrawableRes
 import androidx.test.platform.app.InstrumentationRegistry
+import java.io.File
 
 object TestHelper {
-    fun context() = InstrumentationRegistry.getInstrumentation().targetContext
-    fun packageName() = context().packageName
-    fun tempDirectory() = context().cacheDir
+    fun context(): Context = InstrumentationRegistry.getInstrumentation().targetContext
+    private fun packageName() = context().packageName
+    fun tempDirectory(): File = context().cacheDir
 
     fun drawableUri(@DrawableRes res: Int) = "android.resource://${packageName()}/${res}"
-    fun drawableBitmap(@DrawableRes res: Int, options: BitmapFactory.Options) = BitmapFactory.decodeResource(context().resources, res, options)
+    fun drawableBitmap(@DrawableRes res: Int, options: BitmapFactory.Options): Bitmap = BitmapFactory.decodeResource(context().resources, res, options)
 }
