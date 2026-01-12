3e8511aa3f1fd0776310d60708853df08b7e3fab
Woraphot Chokratanasombat <guhungry@gmail.com>
2026-01-12 23:04:37 +0700

    chore(demo): remove unused styles

--- app/src/main/java/com/guhungry/photomanipulator/demo/ui/theme/Theme.kt
+++ app/src/main/java/com/guhungry/photomanipulator/demo/ui/theme/Theme.kt
@@ -57,7 +57,6 @@ fun MyApplicationTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
